d1461128e9f6958726bd56348b44354cdeb60890
Jennifer Zheng <jez38@pitt.edu>
2020-10-07 16:44:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,10 +40,10 @@ function draw() {
       pipes.splice(i, 1);
     }
     
-      displayScore(pipes[i]);
-      spaceMin++;
+    displayScore(pipes[i]);
+    spaceMin++;
     spaceMax--;
-      space = random(spaceMin, spaceMax);
+    space = random(spaceMin, spaceMax);
   }
 
   if (gameIsOver) {
@@ -70,9 +70,9 @@ function displayScore(pipes){
       if (bird.x > pipes.x && bird.x < pipes.x + pipes.w) {
         score++;
       }
-    }
+  }
   fill(100);
-  text(`Score: ${score}`, 10, 20);
+  text(`Score: ${Math.floor(score/9)}`, 10, 20); //Flooring score/9 makes score increment by 1
 }
 
 class Bird {
@@ -94,10 +94,12 @@ class Bird {
       this.velocity += this.gravity;
       this.velocity *= 0.9;
       this.y += this.velocity;
+      
       if (this.y > height) {
         this.y = height;
         this.velocity = 0;
       }
+      
       if (this.y < 0) {
         this.y = 0;
         this.velocity = 0;
@@ -120,21 +122,19 @@ class Pipe {
     this.highlight = false;
     this.space = random(space);
     this.spaceY = random(35, height - 35);
-    
   }
 
   show() {
     fill(100);
+    
     if (this.highlight == true) {
       fill(0, 100, 100);
     } else{
       fill(35, 100, 100);
     }
     
-    //Can replace with getting a random space in between pipes
-    // and a random starting point for the space for better obstacles
-    //rect(this.x, 0, this.w, this.top)
-    //rect(this.x, height - this.bottom, this.w, this.bottom);
+    //To create better obstacles, replace below with 
+    //getting a random space in between pipes and a random starting point for the space
     rect(this.x, 0, this.w, this.top);
     rect(this.x, height - this.bottom, this.w, this.bottom);
   }
@@ -154,9 +154,6 @@ class Pipe {
   }
 
   hits(bird) {
-    //can be improved
-
-  
     if (bird.y < this.top || bird.y > height - this.bottom) {
       if (bird.x > this.x && bird.x < this.x + this.w) {
         this.highlight = true;
